0aa63afb1a76fde8044cc4a73065825ced07ea19
smeekas <smeetapatel008@gmail.com>
2026-07-04 21:11:56 +0530

    fix: eslint removed fast refresh option

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,6 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
